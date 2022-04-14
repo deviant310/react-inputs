4b5426c8cfdd073bb4dfe4d3c7112d7cb537fe83
Anton Lebedev <admin@foobar.team>
2022-04-15 02:14:57 +0300

    task_1: changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit test two <code>src/App.tsx</code> and save to reload.
+          Edit test two task 1 change 1 and change 2 <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
